50a87f794a526076e19799c6f1163ba36639a8cd
Sankalpbp <sankalpnitj@gmail.com>
2023-05-22 02:29:06 +0530

    Sliding window Day 1

---
+++ java/slidingWindow/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main ( String... args ) {
+
+        int [ ] arr = { 2, 5, 1, 8, 2, 9, 1 };
+        System.out.println ( MaximumSumSubarray.maximumSumSubarray ( arr, 3 ) );
+    }
+}
