c375027e2fa9973fc98c7981627f9dbce2d19819
Sankalpbp <sankalpnitj@gmail.com>
2023-05-22 20:57:29 +0530

    More efficient Anagrams occurrences

--- java/slidingWindow/CountOccurrencesOfAnagrams.java
+++ java/slidingWindow/CountOccurrencesOfAnagrams.java
@@ -16,13 +16,17 @@ public class CountOccurrencesOfAnagrams {
         return true;
     }
 
+    private static void insertInMap ( final Map<Character, Integer> frequency, char ch ) {
+        if ( frequency.containsKey ( ch ) ) {
+            frequency.put ( ch, frequency.get ( ch ) + 1 );
+        } else {
+            frequency.put ( ch, 1 );
+        }
+    }
+
     private static void getFrequency ( final String pattern, final Map<Character, Integer> frequency ) {
         for ( char ch : pattern.toCharArray () ) {
-            if ( frequency.containsKey ( ch ) ) {
-                frequency.put ( ch, frequency.get ( ch ) + 1 );
-            } else {
-                frequency.put ( ch, 1 );
-            }
+            insertInMap ( frequency, ch );
         }
     }
 
@@ -31,39 +35,41 @@ public class CountOccurrencesOfAnagrams {
         int k = pattern.length ();
         final Map<Character, Integer> frequency = new HashMap<> ();
         getFrequency ( pattern, frequency );
-        final Map<Character, Integer> frequencyOfCharacter = new HashMap<> ();
 
         int occurrences = 0;
+        int count = frequency.entrySet ().size ();
 
         int i = 0;
         int j = 0;
 
-        while ( ( j - i + 1 ) < k ) {
-            char ch = s.charAt ( j );
-            if ( frequencyOfCharacter.containsKey ( ch ) ) {
-                frequencyOfCharacter.put ( ch, frequencyOfCharacter.get ( ch ) + 1 );
-            } else {
-                frequencyOfCharacter.put ( ch, 1 );
-            }
-            ++j;
-        }
-
         while ( j < s.length () ) {
             char ch = s.charAt ( j );
-            if ( frequencyOfCharacter.containsKey ( ch ) ) {
-                frequencyOfCharacter.put ( ch, frequencyOfCharacter.get ( ch ) + 1 );
-            } else {
-                frequencyOfCharacter.put ( ch, 1 );
+
+            if ( frequency.containsKey ( ch ) ) {
+                frequency.put ( ch, frequency.get ( ch ) - 1 );
+                if ( frequency.get ( ch ) == 0 ) {
+                    --count;
+                }
             }
 
-            if ( matchFrequency ( frequency, frequencyOfCharacter ) ) {
-                ++occurrences;
+            if ( ( j - i + 1 ) < k ) {
+                ++j;
+            } else if ( ( j - i + 1 ) == k ) {
+                // update the occurrences
+                if ( count == 0 ) {
+                    ++occurrences;
+                }
+
+                // slide the window
+                if ( frequency.containsKey ( s.charAt ( i ) ) ) {
+                    frequency.put ( s.charAt ( i ), frequency.get ( s.charAt ( i ) ) + 1 );
+                    if ( frequency.get ( s.charAt ( i ) ) == 1 ) {
+                        ++count;
+                    }
+                }
+                ++j;
+                ++i;
             }
-
-            frequencyOfCharacter.put ( ch, frequencyOfCharacter.get ( ch ) - 1 );
-
-            ++i;
-            ++j;
         }
 
         return occurrences;
