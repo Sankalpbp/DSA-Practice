6d17a24c89807c09c080a6184b80fb7e62225030
Sankalpbp <sankalpnitj@gmail.com>
2023-07-10 02:29:38 +0530

    2D array in Java

---
+++ java/multidimensional/TwoD.java
@@ -0,0 +1,24 @@
+public class TwoD {
+
+    private static int [][] make ( int n, int m ) {
+        int [][] matrix = new int [ n ][ m ];
+        for ( int i = 0; i < n; ++i ) {
+            matrix [ i ] = new int [ m ];
+            for ( int j = 0; j < m; ++j ) {
+                matrix [ i ][ j ] = i + j;
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void print ( int n, int m ) {
+        int [][] matrix = make ( n, m );
+        for ( int i = 0; i < n; ++i ) {
+            for ( int j = 0; j < m; ++j ) {
+                System.out.print ( matrix [ i ][ j ] + " " );
+            }
+            System.out.println ();
+        }
+    }
+}
