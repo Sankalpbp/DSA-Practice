eeef9addc848ca5dd5f372a31ba72105f4f81177
Sankalpbp <sankalpnitj@gmail.com>
2023-07-10 02:31:28 +0530

    No need to initialize each array in 2D

--- java/multidimensional/TwoD.java
+++ java/multidimensional/TwoD.java
@@ -1,6 +1,6 @@
 public class TwoD {
 
-    private static int [][] make ( int n, int m ) {
+    private static int [][] make2 ( int n, int m ) {
         int [][] matrix = new int [ n ][ m ];
         for ( int i = 0; i < n; ++i ) {
             matrix [ i ] = new int [ m ];
@@ -8,12 +8,11 @@ public class TwoD {
                 matrix [ i ][ j ] = i + j;
             }
         }
-
         return matrix;
     }
 
     public static void print ( int n, int m ) {
-        int [][] matrix = make ( n, m );
+        int [][] matrix = make2 ( n, m );
         for ( int i = 0; i < n; ++i ) {
             for ( int j = 0; j < m; ++j ) {
                 System.out.print ( matrix [ i ][ j ] + " " );
